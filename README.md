9b85509238876d31b1a02999439a513036067508
Uthrom <>
2020-03-07 17:43:17 -0600

    Initial files

---
+++ README.md
@@ -0,0 +1,6 @@
+# Panopticon
+
+Who watches the watchers? The base.
+
+This removes the fog of war from all areas inside a polygon determined by the placement of your radars throughout the base.
+
---
+++ constants.lua
@@ -0,0 +1,7 @@
+local Constants = {}
+
+Constants.ModName = "Panopticon"
+Constants.AssetModName = "__" .. Constants.ModName .. "__"
+Constants.LogFileName = Constants.ModName .. "_logOutput.txt"
+
+return Constants
