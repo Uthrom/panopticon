80ea0847e3076a1367b6fe858a87bf77150d8991
Uthrom <>
2020-03-08 13:31:37 -0500

    Fixed default cycle

--- settings.lua
+++ settings.lua
@@ -3,7 +3,7 @@ data:extend({
     name = "panopticon-rechart-interval",
     type = "int-setting",
     setting_type = "runtime-global",
-    default_value = 30,
+    default_value = 10,
     minimum_value = 10,
     maximum_value = 600,
     order = "1101"
